c02a064b6b48698082e7f98c1d8fa2d235dd3271
nikhila6813 <37750053+nikhila6813@users.noreply.github.com>
2023-03-15 22:11:27 -0400

    Figure out material module dependencies

--- frontend/src/layouts/landing-layout/landing-layout-routing.module.ts
+++ frontend/src/layouts/landing-layout/landing-layout-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from 'src/modules/auth/auth.module';
 import { LandingComponent } from '../../modules/landing/landing.component';
 
 const routes: Routes = [
   { 
     path: '',
-    component: LandingComponent,
     loadChildren: () => import('src/modules/landing/landing.module')
       .then( m => m.LandingModule)
   },
   { 
     path: 'auth',
-    loadChildren: () => import('src/modules/auth/auth-routing.module')
-      .then( m => m.AuthRoutingModule)
+    loadChildren: () => import('src/modules/auth/auth.module')
+      .then( m => m.AuthModule)
   },
   { 
     path: 'aboutus',
-    loadChildren: () => import('src/modules/aboutUs/aboutus-routing.module')
-      .then( m => m.AboutUsRoutingModule)
+    loadChildren: () => import('src/modules/aboutUs/aboutus.module')
+      .then( m => m.AboutUsModule)
   }
 ];
 
